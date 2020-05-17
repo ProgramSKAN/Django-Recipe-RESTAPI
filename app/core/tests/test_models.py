1ede4393a7c87e8513eff4c7daaa77d5a4cd9387
ProgramSKAN <kandagatlas@gmail.com>
2020-05-17 11:04:04 +0530

    Added Normalized email feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,4 +14,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email,email)
-        self.assertTrue(user.check_password(password))
+        self.assertTrue(user.check_password(password))    
+    
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized(as the domain of email is case insensitive)"""
+        email='user@GMAIL.COM'
+        user=get_user_model().objects.create_user(email,'user1234')
+        
+        self.assertEqual(user.email,email.lower())
