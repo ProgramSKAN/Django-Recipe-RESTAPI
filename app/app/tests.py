42ad00d7d01e5c0447bf0c7b486cc05c16fae640
ProgramSKAN <kandagatlas@gmail.com>
2020-05-17 08:48:40 +0530

    Test driven development TDD: write test before code
    witten subtract test before subtract function

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,7 +2,7 @@
 #for testing: all the files,folders,test function must begin with 'test'
 
 from django.test import TestCase
-from app.calculator import add
+from app.calculator import add,subtract
 
 
 class CalcTests(TestCase):
@@ -10,3 +10,7 @@ class CalcTests(TestCase):
     def test_add_numbers(self): 
         """Test that two numbers are added together"""
         self.assertEqual(add(3,8),11)
+    
+    def test_subtract_numbers(self):
+        """Test that values are subtracted and returned"""
+        self.assertEqual(subtract(5,11),6)
